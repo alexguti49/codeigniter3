2ed86ea077daaa3accc796a44f3ba75fd27e1c46
alexguti49 <luisgutidrive@hotmail.com>
2017-02-24 22:46:14 -0500

    Aplicacion de Botstrap-validator

--- application/views/cursos/formulario.php
+++ application/views/cursos/formulario.php
@@ -7,8 +7,8 @@
 <div class="container-fluid text-left">    
     <div class="row content">
         <div class="col-sm-offset-3 col-sm-6 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4 col-xs-offset-0 col-xs-12">
+        <h2>Registro de Cursos</h2>
         
-        <div class="form-group">
             <?= form_open("/cursos/recibirDatos") ?>
             <?
                 $nombre = array(
@@ -30,19 +30,22 @@
                     
                 );
             ?>
-
-            <?= form_label('Nombre: ', 'nombre') ?>
-            <?= form_input($nombre) ?>
-            </br>
-            <?= form_label('Cantidad de Videos: ', 'videos') ?>
-            <?= form_input($videos) ?>
-            </br>
+            <div class="form-group">
+                <?= form_label('Nombre: ', 'nombre') ?>
+                <?= form_input($nombre) ?>
+            </div>
+            
+            <div class="form-group">
+                <?= form_label('Cantidad de Videos: ', 'videos') ?>
+                <?= form_input($videos) ?>
+            </div>
+            
             <center>
                 <?= form_submit($añadir) ?>
             </center>
             
             <?= form_close() ?>
-        </div>
+        <!--form-->
 
         </div>
     </div>
